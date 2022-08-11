abd89ae61bd65bdc939ae8e175cdc2068289ea45
Caroline <89542446+CarolFenixBr@users.noreply.github.com>
2022-08-11 15:13:19 -0300

    Update Aula2.c

--- C/Aula2.c
+++ C/Aula2.c
@@ -10,9 +10,9 @@
   idade = 20;
   salario = 3800.5;
   altura = 1.63;
-  genero = 'F';
-    
+  genero = 'F';    
   strcpy(nome, "Caroline");
+    
     printf("IDADE = %d\n", idade);
     printf("SALARIO = %.2lf\n", salario);
     printf("ALTURA = %.2lf\n", altura);
