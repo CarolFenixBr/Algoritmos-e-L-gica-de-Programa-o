8d7a2bbdc1daa78bf500090f94d8fffc4a658b98
Caroline <89542446+CarolFenixBr@users.noreply.github.com>
2022-08-13 00:07:58 -0300

    Create Aula02.py

---
+++ Python/Aula02.py
@@ -0,0 +1,16 @@
+idade: int
+salario: float; altura: float
+genero: str
+nome: str
+  
+idade = 28
+salario = 3800.5
+altura = 1.63
+genero = "F"
+nome = "Caroline Vargas"
+
+print(f"IDADE = {idade}")
+print(f"SALARIO = {salario:.2f}")
+print(f"ALTURA = {altura:.2f}")
+print(f"GENERO = {genero}")
+print(f"NOME = {nome}")
