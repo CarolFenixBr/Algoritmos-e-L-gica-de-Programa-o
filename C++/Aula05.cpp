18d21ae209e893081d7496d3ae58b2cec4c03003
Caroline <89542446+CarolFenixBr@users.noreply.github.com>
2022-08-12 22:20:07 -0300

    Create Aula05.cpp

---
+++ C++/Aula05.cpp
@@ -0,0 +1,15 @@
+#include <iomanip>
+
+using namespace std;
+
+int main()
+{
+
+  int a, b, resultado;
+  a = 5;
+  b = 2;
+  resultado = (double)a / b;
+  cout << resultado << endl;
+  
+  return 0;
+}
